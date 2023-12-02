346b2e0a565c2aa318c1836bf0e8cae0c118dc37
Fabrice Dugas <fabby.dugas@gmail.com>
2023-12-02 14:28:04 -0500

    Cleanup day 2

--- day2/day2.go
+++ day2/day2.go
@@ -25,50 +25,6 @@ func main() {
 	part2()
 }
 
-func parseDraw(drawString string) []int {
-	draw := make([]int, 3)
-	tokens := strings.Split(drawString, ",")
-	for _, colorString := range tokens {
-		colorString = strings.TrimSpace(colorString)
-		colorTokens := strings.Split(colorString, " ")
-		count, err := strconv.Atoi(colorTokens[0])
-		utils.Check(err)
-		color := colorTokens[1]
-		draw[colors[color]] = count
-	}
-
-	return draw
-}
-
-func parseGame(line string) (gameId int, draws [][]int) {
-	tokens := strings.Split(line, ":")
-	gameString := tokens[0]
-	drawsString := tokens[1]
-
-	tokens = strings.Split(gameString, " ")
-	gameId, err := strconv.Atoi(tokens[1])
-	utils.Check(err)
-
-	tokens = strings.Split(drawsString, ";")
-
-	draws = make([][]int, 0)
-	for _, drawString := range tokens {
-		draw := parseDraw(drawString)
-
-		// logDraw(draw)
-
-		draws = append(draws, draw)
-	}
-
-	return gameId, draws
-}
-
-func logDraw(draw []int) {
-	fmt.Printf("Red : %d\n", draw[red])
-	fmt.Printf("Green : %d\n", draw[green])
-	fmt.Printf("Blue : %d\n", draw[blue])
-}
-
 func part1() {
 	file, err := os.Open("day2-input.txt")
 	utils.Check(err)
@@ -110,28 +66,47 @@ func part2() {
 	for scanner.Scan() {
 		line := scanner.Text()
 		fmt.Println(line)
-
 		_, draws := parseGame(line)
-
-		maxCubes := make([]int, 3)
-
-		for _, draw := range draws {
-			for color, count := range draw {
-				if maxCubes[color] < count {
-					maxCubes[color] = count
-				}
-			}
-		}
-
-		power := 1
-		for _, maxCube := range maxCubes {
-			power *= maxCube
-		}
-
-		sum += power
+		maxCubes := findMaxCubes(draws)
+		sum += computePower(maxCubes)
 	}
 
 	fmt.Printf("Solution : %d\n", sum)
+}
+
+func parseDraw(drawString string) []int {
+	draw := make([]int, 3)
+	tokens := strings.Split(drawString, ",")
+	for _, colorString := range tokens {
+		colorString = strings.TrimSpace(colorString)
+		colorTokens := strings.Split(colorString, " ")
+		count, err := strconv.Atoi(colorTokens[0])
+		utils.Check(err)
+		color := colorTokens[1]
+		draw[colors[color]] = count
+	}
+
+	return draw
+}
+
+func parseGame(line string) (gameId int, draws [][]int) {
+	tokens := strings.Split(line, ":")
+	gameString := tokens[0]
+	drawsString := tokens[1]
+
+	tokens = strings.Split(gameString, " ")
+	gameId, err := strconv.Atoi(tokens[1])
+	utils.Check(err)
+
+	tokens = strings.Split(drawsString, ";")
+
+	draws = make([][]int, 0)
+	for _, drawString := range tokens {
+		draw := parseDraw(drawString)
+		draws = append(draws, draw)
+	}
+
+	return gameId, draws
 }
 
 func isPossibleDraw(draw []int) bool {
@@ -142,4 +117,26 @@ func isPossibleDraw(draw []int) bool {
 	}
 
 	return true
+}
+
+func findMaxCubes(draws [][]int) []int {
+	maxCubes := make([]int, 3)
+
+	for _, draw := range draws {
+		for color, count := range draw {
+			if maxCubes[color] < count {
+				maxCubes[color] = count
+			}
+		}
+	}
+
+	return maxCubes
+}
+
+func computePower(cubeset []int) int {
+	power := 1
+	for _, count := range cubeset {
+		power *= count
+	}
+	return power
 }
